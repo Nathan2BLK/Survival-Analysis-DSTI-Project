08ac11a38e8ffffb07b6974af36c2a02a9a46483
Joël Bolduc <joel.etienne.bolduc@gmail.com>
2025-02-26 22:04:20 +0100

    Created a cox model that accounts for the different columns
    Created a cox model that accounts for the different columns

--- gym.R
+++ gym.R
@@ -1,7 +1,9 @@
 library(tidyverse)
+library(ggsurvfit)
+library(dplyr)
+library(survival)
+library(survminer)
 setwd('C:/Users/joelb/Desktop/survival_analysis')
 gym_data <- read_csv('gym_churn_us.csv')
 
-res_cox <- coxph(formula = Surv(Lifetime, Churn) ~ gender + Near_Location + Partner + Promo_friends + Phone + Contract_period + Group_visits + Age + Avg_additional_charges_total + Month_to_end_contract + Avg_class_frequency_total + Avg_class_frequency_current_month, data =  gym_data)
-summary(res_cox)
-ggsurvplot(survfit(res_cox, data = gym_data), color = "#2E9FDF",ggtheme = theme_minimal())
+coxph(Surv(gym_data$Lifetime,gym_data$Churn) ~ gym_data$gender+gym_data$Near_Location+gym_data$Partner+gym_data$Promo_friends+gym_data$Phone+gym_data$Contract_period+gym_data$Group_visits+gym_data$Age+gym_data$Avg_additional_charges_total+gym_data$Month_to_end_contract+gym_data$Avg_class_frequency_total+gym_data$Avg_class_frequency_current_month, data = lung)
